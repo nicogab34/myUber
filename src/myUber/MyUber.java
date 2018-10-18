fc305055c85082f99c1d99da8097fc354307ad59
GABRION Nicolas <nico.gabrion@gmail.com>
2018-10-18 10:58:59 +0200

    -

--- src/myUber/MyUber.java
+++ src/myUber/MyUber.java
@@ -11,7 +11,10 @@ public class MyUber {
 		
 		
 		Van v1 = new Van(new ArrayList<Driver>(Arrays.asList(d1,d2)));
-
+		Standard s1 = new Standard(new ArrayList<Driver>(Arrays.asList(d2)));
+		Berline b1 = new Berline(new ArrayList<Driver>(Arrays.asList(d1)));
+		
+		Customer c1 = new Customer("Baptiste", "Andrieu", new ArrayList<Float>(Arrays.asList(3.0,4.0)), 125765894);
 	}
 
 }
