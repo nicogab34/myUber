3e31c4e0c6d62de92c3d5c6d35c58cdbd13cf77b
Vincent Bouget <vincent-bouget@hotmail.fr>
2018-11-03 17:29:23 +0100

    aa

--- src/myUber/RideFactory.java
+++ src/myUber/RideFactory.java
@@ -5,16 +5,16 @@ public class RideFactory {
 	public Ride createRide(String Ridetype,String state,Customer customer,int ID, Driver driver) {
 		
 		if(Ridetype.equalsIgnoreCase("UberX")) {
-			return (new UberX(state,customer,ID,driver));
+			return (new UberX(customer,ID,driver));
 		}
 		if(Ridetype.equalsIgnoreCase("UberBlack")) {
-			return (new UberVan(state,customer,ID,driver));
+			return (new UberVan(customer,ID,driver));
 		}
 		if(Ridetype.equalsIgnoreCase("UberVan")) {
-			return (new UberVan(state,customer,ID,driver));
+			return (new UberVan(customer,ID,driver));
 		}
 		if(Ridetype.equalsIgnoreCase("UberPool")) {
-			return (new UberPool(state,customer,ID,driver));
+			return (new UberPool(customer,ID,driver));
 		}
 		return(null);
 	}
